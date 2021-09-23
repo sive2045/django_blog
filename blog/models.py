715ae1d1e01440dd8a9a3a97358c80b459c99bc8
sive2045 <lc9902130509@gmail.com>
2021-09-23 22:02:09 +0900

    삭제된 유저는 None으로 표시하기

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 class Post(models.Model):
@@ -11,11 +12,12 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # author : 추후 업데이트
+    
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
 
     def __str__(self):
-        return f'[{self.pk}] {self.title}'
+        return f'[{self.pk}] {self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
