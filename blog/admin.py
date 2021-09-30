33659a77ac8b5f7005c0c4c876c83997593d4dc9
sive2045 <lc9902130509@gmail.com>
2021-09-30 15:51:53 +0900

    Add comment function in post content

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from markdownx.admin import MarkdownxModelAdmin
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -11,3 +11,4 @@ class TagAdmin(admin.ModelAdmin):
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Comment)
--- blog/tests.py
+++ blog/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User
 from bs4 import BeautifulSoup
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 class TestView(TestCase):
     def setUp(self):
@@ -45,6 +45,12 @@ class TestView(TestCase):
         )
         self.post_003.tags.add(self.tag_ptyhon)
         self.post_003.tags.add(self.tag_python_kor)
+
+        self.comment_001 = Comment.objects.create(
+            post=self.post_001,
+            author = self.user_sive,
+            content='첫 번째 댓글입니다.'
+        )
     
     def category_card_test(self, soup):
         categories_card = soup.find('div', id='categories-card')
@@ -141,6 +147,12 @@ class TestView(TestCase):
         self.assertIn(self.tag_hello.name, post_area.text)
         self.assertNotIn(self.tag_python_kor.name, post_area.text)
         self.assertNotIn(self.tag_ptyhon.name, post_area.text)
+
+        # comment area
+        comments_area = soup.find('div', id='comment-area')
+        comment_001_area = comments_area.find('div', id='comment-1')
+        self.assertIn(self.comment_001.author.username, comment_001_area.text)
+        self.assertIn(self.comment_001.content, comment_001_area.text)
     
     def test_category_page(self):
         response = self.client.get(self.category_programming.get_absolute_url())
