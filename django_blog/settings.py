c1c08fce934e0c158323bfca6a2efd3add38af44
sive2045 <lc9902130509@gmail.com>
2021-10-15 00:24:42 +0900

    make docker image

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5)#vgpw%232^l!9$kkaug2l_(owe-k42#&&$g6-eiibjwhc)1j'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5)#vgpw%232^l!9$kkaug2l_(owe-k42#&&$g6-eiibjwhc)1j')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
 
-ALLOWED_HOSTS = []
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
+
 
 
 # Application definition
