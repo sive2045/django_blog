88dbb07bac6cff27d4fb72f366717355d3a01036
Chungnyeong Lee <50041580+sive2045@users.noreply.github.com>
2021-11-02 23:16:51 +0900

    feat: Create db for Todo Todo에 필요할 db 정의함 Todo django admin에 등록함

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdown
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=30)
+    content = MarkdownxField()
+    deadline = models.DateField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_complete = models.BooleanField(blank=True, null=True)
+
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title} :: {self.author}'
+    
+    def get_absolute_url(self):
+        return f'/todo/{self.author}/{self.pk}/'
+
+    def get_content_markdown(self):
+        """
+        마크다운으로 작성한 content를
+        HTML로 변환해줌
+        """
+        return markdown(self.content)
--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+from bs4 import BeautifulSoup
 
-# Create your tests here.
+class TestTodo(TestCase):
+    def setup(self):
+        """
+        사전 db 처리
+        """
+        self.client = Client()
+        self.user_sive = User.objects.create_user(username="sive", password="asdfqwer1234")
+        self.user_sive.is_staff = True
+        self.user_sive.save()
+
