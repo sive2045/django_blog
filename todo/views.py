e50a761e8eab1f1e1450cc9c98f6d7056d2826c9
Chungnyeong Lee <50041580+sive2045@users.noreply.github.com>
2021-11-10 00:04:01 +0900

    feat: todo view class todo생성하는 페이지 만듦 수정해얄 것 : todo create submit 후 redirect page 지정, form에 dead line 달력 폼으로 입력하기

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.generic import DeleteView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from .models import Todo
 
 def index(request):
+    """
+    main page update 예정
+    """
     return render(
         request,
         'todo/base.html'
     )
+
+class TodoCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Todo
+    fields = ['title', 'content', 'deadline', 'is_complete']
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+    
+    def form_vaild(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_superuser or current_user.is_staff):
+            form.instance.author = current_user
+            response = super(TodoCreate,self).form_vaild(form)
+            return response
+        else:
+            return redirect('/todo/')
