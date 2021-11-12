7210c5a54b1a36b8d153127d52022e62967546db
Chungnyeong Lee <50041580+sive2045@users.noreply.github.com>
2021-11-13 00:21:37 +0900

    feat: todo 생성 생성 및 수정 기능 추가 todo: 작성자명 자동 기입안되는 버그 수정해야함

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('create_todo/', views.TodoCreate.as_view()),
+    path('update_todo/<int:pk>/', views.TodoUpdate.as_view()),
 ]
 
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.views.generic import DeleteView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
 
 from .models import Todo
 
@@ -16,15 +17,32 @@ def index(request):
 class TodoCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Todo
     fields = ['title', 'content', 'deadline', 'is_complete']
+    success_url = '/todo/'
 
     def test_func(self):
         return self.request.user.is_superuser or self.request.user.is_staff
     
     def form_vaild(self, form):
-        current_user = self.request.user
+        current_user = self.request.user.username
         if current_user.is_authenticated and (current_user.is_superuser or current_user.is_staff):
             form.instance.author = current_user
             response = super(TodoCreate,self).form_vaild(form)
             return response
         else:
-            return redirect('/todo/')
+            return redirect('/todo/')        
+
+class TodoUpdate(LoginRequiredMixin, UpdateView):
+    model = Todo
+    fields = ['title', 'content', 'deadline', 'is_complete']
+    success_url = '/todo/'
+    template_name = 'todo/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(TodoUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+    
+    def form_valid(self, form):
+        response = super(TodoUpdate, self).form_vaild(form)
+        return response
